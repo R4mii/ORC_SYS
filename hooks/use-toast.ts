fc0bfd264456306c92852e1e2fb1e1964da66f64
unknown <R4mii>
2025-04-17 13:31:06 +0100

    finalizing the (achats) upload section v5

--- hooks/use-toast.ts
+++ hooks/use-toast.ts
@@ -52,7 +52,11 @@ export const ToastProvider = ({ children }: { children: React.ReactNode }) => {
     }
   }, [toasts, dismiss])
 
-  return <ToastContext.Provider value={{ toasts, toast, dismiss }}>{children}</ToastContext.Provider>
+  return (
+    <ToastContext.Provider value={{ toasts, toast, dismiss }}>
+      {children}
+    </ToastContext.Provider>
+  )
 }
 
 export function useToast() {
