adec59eeae3c160fc71233019584dc65be927927
unknown <R4mii>
2025-04-20 19:09:23 +0100

    fix: deployment error

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,6 @@ import { metadata } from "./metadata"
 
 const inter = Inter({ subsets: ["latin"] })
 
-// Only export metadata once - it's already imported from ./metadata
 export { metadata }
 
 export default function RootLayout({
